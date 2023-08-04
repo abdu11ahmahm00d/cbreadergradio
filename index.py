caaf83790f4ec65d5ad8ac28dde136d17d5e72b6
Abdullah Mahmood <122725447+abdu11ahmahm00d@users.noreply.github.com>
2023-08-04 19:22:11 +0000

    fin

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 import gradio as gr
-from gradio.components import File, Gallery
+from gradio.components import File, Gallery, Image
 from PIL import Image as PILImage
 import rarfile
 import zipfile
 import os
+import numpy as np
 
 def read_comic_book(comic_file):
     with open(comic_file.name, "wb") as f:
@@ -14,21 +15,21 @@ def read_comic_book(comic_file):
         with rarfile.RarFile(comic_file.name) as archive:
             for entry in archive.infolist():
                 if entry.filename.lower().endswith(('jpg', 'jpeg', 'png')):
-                    image = PILImage.open(archive.open(entry))
-                    images.append(image)
+                    image = Image.open(archive.open(entry))
+                    images.append(np.array(image))
     elif comic_file.name.lower().endswith('.cbz'):
         with zipfile.ZipFile(comic_file.name) as archive:
             for entry in archive.infolist():
                 if entry.filename.lower().endswith(('jpg', 'jpeg', 'png')):
-                    image = PILImage.open(archive.open(entry))
-                    images.append(image)
+                    image = Image.open(archive.open(entry))
+                    images.append(np.array(image))  
     
     return images
 
 iface = gr.Interface(
     fn=read_comic_book,
     inputs=File(type="file"),
-    outputs=Gallery(type="numpy", plot=False),
+    outputs=Gallery(),  # Removed type and plot parameters
     live=True,
     examples=[["/workspaces/cbreadergradio/examples/KTR2.cbr"], ["/workspaces/cbreadergradio/examples/KTR1.cbz"]],
     allow_flagging='never'
